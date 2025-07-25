031ae47f50a9560494da21c5ec959b48e7bb5e64
Joao Marcello <joaomarcello.mm@gmail.com>
2025-07-25 11:06:18 -0300

    auto detect period

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -2,11 +2,22 @@ import os
 import torch
 import numpy as np
 
+from data_provider.data_factory import data_provider
+from utils.fft_utils import detect_period_fft
 
 class Exp_Basic(object):
     def __init__(self, args):
         self.args = args
         self.device = self._acquire_device()
+
+        if self.args.period <= 0:
+            window = self.args.seq_len
+            dataset, _ = data_provider(self.args, flag='train')
+            data_x = dataset.data_x
+            data_x = dataset.data_x[:window].copy()
+            self.args.period = detect_period_fft(data_x)
+            print(f"Período detectado por FFT: {self.args.period}")
+
         self.model = self._build_model().to(self.device)
 
     def _build_model(self):
